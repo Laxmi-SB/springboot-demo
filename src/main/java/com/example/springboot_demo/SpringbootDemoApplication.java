44541d0775052945db10a43ab63f6ada7ca0bd31
Anmolsra <anmolraiwade@gmail.com>
2025-12-30 12:17:43 +0530

    Initial commit: Spring Boot demo application

---
+++ src/main/java/com/example/springboot_demo/SpringbootDemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.springboot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class SpringbootDemoApplication {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title",
+                "I have successfully built a Spring Boot application using Maven");
+        model.addAttribute("msg",
+                "This application can be deployed to Kubernetes using Argo CD");
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+}
